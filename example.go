3a2e6566998e50dcef0b0133d22bb9ddafb67f65
Burak Serdar <brxd@cloudprivacylabs.com>
2024-08-18 20:52:33 -0600

    Remove deps

--- example.go
+++ example.go
@@ -2,10 +2,8 @@ package example
 
 import (
 	"fmt"
-
-	_ "github.com/spf13/viper"
 )
 
 func Example() {
-	fmt.Println("This is package example of module mycompany.com/mymodule")
+	fmt.Println("This is package example of module github.com/bserdar/go-recipes-module")
 }
